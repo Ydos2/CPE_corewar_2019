86d3c65e4ca01a194c755455b9d540197fcc05d9
mathias <mathias.ressort@epitech.eu>
2020-05-16 15:01:34 +0200

    [Add] Tests file .s

--- bonus/src/test_file.s
+++ bonus/src/test_file.s
@@ -1,40 +1,15 @@
-    .name "ristas"
-    .comment "get issoued"
+#
+# zork . s for corewar
+#
+# Bob Bylan
+#
+# Sat Nov 10 03:24;30 2081
+#
+.name "zork"
+.comment "just a basic living program"
 
-    ld %1, r2
-    ld %3, r3
-    st r1 r2
-    sti r1,  %:issou_protect, %1
-    sti r1, %:attack, %1
-issou_protect:
-    live %84
-    ld %150 r4
-    sub r3, r4, r4
-    fork r4
-attack: ld %80, r3
-    live %84
-    ld %50, r3
-    ld    30, r4
-    sub r3, r4, r3
-    live %84
-    st r3, 162
-    st r3, 173
-    st r3, 184
-    st r3, 195
-    st r3, 206
-    ld 100, r4
-    zjmp %:issou_protect
-    add r3, r4, r3
-    st r3, 127
-    st r3, 129
-    st r3, 184
-    st r3, 195
-    st r3, 206
-    zjmp %:attack
-
-issou_shield:
-    ld %30, r4
-    live %84
-    sub r3, r4, r7
-    fork r7
-    live %84
+l2:
+sti r1, %:live, %1   ### kuhc
+and r1, %0, r1#kjdchfu
+live: live %1
+zjmp %:live
